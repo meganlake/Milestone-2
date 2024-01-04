3bacff71db5d569f618327c67be3cbc40e322258
Christian Alquisada <calquisada@aol.com>
2024-01-03 21:15:47 -0800

    commit

---
+++ views/wishlist/edit.jsx
@@ -0,0 +1,33 @@
+const React = require('react')
+const Default = require('../default.jsx')
+
+function edit_form (data) {
+    return (
+        <Default>
+            <main>
+                <h1>Edit Wishlist</h1>
+                <form method="POST" action={`/wishlist/${data.place.id}?_method=PUT`}>
+                    <div className=''>
+                        <label htmlFor='pic'>Place Picture</label>
+                        <input className='' id='pic' name='pic' value={data.place.pic} />
+                        </div>
+                    <div className=''>
+                        <label htmlFor='country'>Country</label>
+                        <input className='' id='country' name='country' value={data.place.country} />
+                    </div>
+                    <div className=''>
+                        <label htmlFor='city'>City</label>
+                        <input className='' id='city' name='city' value={data.place.city} />
+                    </div>
+                    <div className=''>
+                        <label htmlFor='rating'>Rating</label>
+                        <input className='' id='rating' name='rating' value={data.place.rating} />
+                    </div>
+                        <input className='' type='submit' value='Update Wishlist' />
+                </form>
+            </main>
+        </Default>
+    )
+}
+
+module.exports = edit_form
