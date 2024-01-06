97641b536bf0eb32167506318e970fc75116092f
Megan Lake <meganlake95@gmail.com>
2024-01-05 16:27:31 -0800

    models and controllers

---
+++ controllers/wishlist_controller.js
@@ -0,0 +1,80 @@
+const wishlists = require('express').Router()
+const db = require('../models')
+
+//Home Page
+wishlists.get('/', (req, res) => {
+    db.Wishlist.find()
+    .then((wishlists) => {
+        res.render('wishlist/all', { wishlists })
+    })
+    .catch(err => {
+        console.log(err)
+        res.render('error404')
+    })
+})
+
+//View New Page
+wishlists.get('/new', (req, res) => {
+    res.render('wishlist/new')
+})
+
+//Add New Favorite
+wishlists.post('/', (req, res) => {
+    db.Wishlist.create(req.body)
+        .then(() => {
+            res.redirect('/wishlist')
+        })
+        .catch((err) => {
+            console.log('err', err)
+            res.render('error404')
+        })
+})
+
+//View Favorites
+wishlists.get('/:id', (req, res) => {
+    db.Wishlist.findById(req.params.id)
+        .then((wishlist) => {
+            res.render('wishlist/show', { wishlist })
+        })
+        .catch((err) => {
+            console.log('err', err)
+            res.render('error404')
+        })
+})
+
+//View Edit Page
+wishlists.get('/:id/edit', (req, res) => {
+    db.Wishlist.findById(req.params.id)
+        .then((wishlist) => {
+            res.render('wishlist/edit', { wishlist })
+        })
+        .catch(err => {
+            res.render('error404')
+        })
+})
+
+//Edit a Favorite
+wishlists.put('/:id', (req, res) => {
+    db.Wishlist.findByIdAndUpdate(req.params.id, req.body)
+        .then(() => {
+            res.redirect(`/wishlist/${req.params.id}`)
+        })
+        .catch((err) => {
+            console.log('err', err)
+            res.render('error404')
+        })
+})
+
+//Delete a Favorite
+wishlists.delete('/:id', (req, res) => {
+    db.Wishlist.findByIdAndDelete(req.params.id)
+        .then(() => {
+            res.redirect('/wishlist')
+        })
+        .catch((err) => {
+            console.log('err', err)
+            res.render('error404')
+        })
+})
+
+module.exports = wishlists
---
+++ models/wishlist.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+const wishlistSchema = new mongoose.Schema({
+    country: { type: String, required: true },
+    city: { type: String },
+    pic: { type: String, defaultValue: 'https://images.unsplash.com/photo-1543872084-c7bd3822856f?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'},
+    rating: { type: Number, required: true }
+})
+
+module.exports = mongoose.model('Wishlist', wishlistSchema)
