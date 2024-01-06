9083789cdfa38fd2c92225dbcaec63da9c8d77b8
Cam <farrcameron@gmail.com>
2024-01-06 10:33:20 -0800

    Added Edit functions, Fixed keywords.

--- views/favorites/show.jsx
+++ views/favorites/show.jsx
@@ -1,4 +1,5 @@
 const React = require('react')
+const Default = require('../default.jsx')
 
 function Show ({location}) {
     return (
@@ -6,7 +7,7 @@ function Show ({location}) {
             <h3>{location.name}</h3>
             <img src={location.image} alt={location.name} />
             <a href={`/favorites/${location.id}/edit`}><button>Edit</button></a>
-            <form action={`/favorites/${fravorite.id}?_method=DELETE`} method="POST">
+            <form action={`/favorites/${location.id}?_method=DELETE`} method="POST">
                 <input type='submit' value="DELETE"/>
             </form>
             <li><a href="/default.jsx">Go home</a></li>
