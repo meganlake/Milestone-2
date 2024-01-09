20f4dff5612710fabab7501f3615f228a153c479
Cam <farrcameron@gmail.com>
2024-01-08 18:16:50 -0800

    Node updates

--- views/favorites/all.jsx
+++ views/favorites/all.jsx
@@ -1,1 +1,33 @@
 const React = require('react')
+const Default = require('../default.jsx')
+
+function index (data) {
+  let locationFormatted = data.location.map((country, index) => {
+    return (
+      <div className=''>
+        <h2>
+          <a href={`/location/${country.id}`} >
+            {country.name}
+          </a>
+        </h2>
+        <p className=''>
+          {city.name}
+        </p>
+        <img src={country.pic} alt={country.name} />
+      </div>
+    )
+  })  
+  return (
+    <Default>
+        <main>
+            <h1>My Favorite Places!</h1>
+            <div className=''>
+              {locationFormatted}
+            </div>
+        </main>
+    </Default>
+  )
+}
+
+  
+module.exports = index
